07026abfdbe3a6d28bbbdd58d6a4d7f25327ee79
Shreya Birmecha <shreyabirmecha8@gmail.com>
2025-12-13 22:15:49 +0530

    Initial Go project structure

---
+++ cmd/balancer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Smart Adaptive Load Balancer starting...")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sbirmecha99/smart-adaptive-load-balancer
+
+go 1.22.2
